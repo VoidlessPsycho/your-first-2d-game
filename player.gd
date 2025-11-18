8a723b5afa1b4d108c999ce45288cd8247d6a101
Voidless_Psycho <mgollapudi104@gmail.com>
2025-11-18 11:10:15 -0800

    Fixed Score Timer
    the score now works!!!

--- player.gd
+++ player.gd
@@ -38,7 +38,7 @@ func _process(delta) -> void:
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(_body) -> void:
 	hide()
 	hit.emit()
 	$CollisionShape2D.set_deferred("disabled", true)
